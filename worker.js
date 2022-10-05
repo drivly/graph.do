424b562019d8d9f240e77d2738dbe5831d58c3ab
Nathan Clevenger <nateclev@gmail.com>
2022-10-05 05:59:22 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,8 +7,14 @@ export const api = {
   url: 'https://graph.do/api',
   type: 'https://apis.do/database',
   endpoints: {
-    listCategories: 'https://graph.do/api',
-    getCategory: 'https://graph.do/:type',
+    resources: 'https://graph.do',
+    list: 'https://graph.do/:resource',
+    search: 'https://graph.do/:resource?prop=value',
+    new: 'https://graph.do/:resource/new?prop=value',
+    get: 'https:/graph.do/:resource/:id',
+    set: 'https://graph.do/:resource/:id/set?prop=value',
+    import: 'https://graph.do/:resource/:id/import/:url',
+    delete: 'https://graph.do/:resource/:id/delete',
   },
   site: 'https://graph.do',
   login: 'https://graph.do/login',
@@ -27,11 +33,7 @@ export const examples = {
 }
 
 export default {
-  fetch: (req, env) => {
-    const { hostname, pathname } = new URL(req.url)
-    const db = pathname.split('/')[1] 
-    return env.GRAPH.get(env.GRAPH.idFromName(hostname + '/' + db)).fetch(req)
-  }
+  fetch: (req, env) => env.GRAPH.get(env.GRAPH.idFromName(new URL(req.url).hostname)).fetch(req)
 }
 
 export class Graph {
@@ -51,11 +53,36 @@ export class Graph {
 //     })
   }
   async fetch(req) {
-    const { pathname, search, searchParams } = new URL(req.url)
-    const options = Object.fromEntries(searchParams)
-    const country = decodeURI(pathname.split('/')[1])
-    const data = country ? await this.state.storage.get(country) : 
-                           await this.state.storage.list(options).then(list => Object.fromEntries(list)) 
-    return new Response(JSON.stringify({ data }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+    const { user, origin, requestId, method, body, time, pathname, pathSegments, query = {} } = await this.env.CTX.fetch(req).then(res => res.json())
+    if (pathname == '/api') return json({ api, gettingStarted, examples, user })
+    if (!user.authenticated) return Response.redirect(origin + '/login')
+    
+    let [ resource, id, action ] = pathSegments
+    action = action ? action : body ? 'set' : id == 'new' ? 'set' : 'get'
+    id = id == 'new' ? crypto.randomUUID() : id
+    
+    query.limit = query.limit ? parseInt(query.limit) : 1000
+    query.reverse = query.reverse ? query.reverse === 'true' : false
+    const start = query.start ?? query.startAfter === undefined
+    const end = (query.start ?? query.startAfter === undefined) && query.reverse
+    
+    let data = id ? await this.state.storage.get(id) :  await this.state.storage.list(query).then(list => Object.fromEntries(list)) 
+    if (query.reverse) data = data.reverse()
+    
+    let links = id ? {
+      self: `${origin}/${resource}/${id}`,
+      set: data?.localTime || query?.localTime ? `${origin}/${resource}/${id}/set?inCity=${user.city}` : `${origin}/${resource}/${id}/set?localTime=${user.localTime}`,
+      delete: `${origin}/${resource}/${id}/delete`
+    } : {
+      self: req.url,
+      start: `${origin}/${resource}`,
+      next: `${origin}/${resource}?startAfter=`,
+      prev: `${origin}/${resource}?reverse=true&end=`,
+      last: `${origin}/${resource}?reverse=true`,
+    }
+    
+    return json({ api, links, data, user })
   }
 }
+
+export const json = data => new Response(JSON.stringify(data, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
